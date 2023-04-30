755575586cf544997a196600ed0bfc43d1197163
mikael-ayala <mikael.ayala.santos@gmail.com>
2023-04-30 16:49:27 -0300

    estilização css do ingresso finalizada, junto com ajustes finais no site

--- js/ingresso.js
+++ js/ingresso.js
@@ -5,12 +5,29 @@ const diaNascimento = dataNascimento.getUTCDate()
 const mesNascimento = dataNascimento.getUTCMonth() + 1
 const anoNascimento = dataNascimento.getUTCFullYear()
 
-const ingresso = document.querySelector('.ingresso-comprado')
+const setor = infoIngresso.tickets.charAt(0).toUpperCase() + infoIngresso.tickets.slice(1)
+let setorAjustado = setor.replace('-', ' ')
 
-console.log(mesNascimento)
+const ingresso = document.querySelector('.ingresso-comprado__div')
 
-ingresso.innerHTML = `
-    Nome: ${infoIngresso.nome}
-    Data de Nascimento: ${diaNascimento}/${mesNascimento}/${anoNascimento} 
-    Setor: ${infoIngresso.tickets}
+const dataHoje = new Date()
+let diaHoje = dataHoje.getUTCDate()
+let mesHoje = dataHoje.getUTCMonth()+1
+
+if (diaHoje <= 9) diaHoje = '0' + diaHoje
+if (mesHoje <= 9) mesHoje = '0' + mesHoje
+
+ingresso.innerHTML += `
+<div class="ingresso-comprado__div__ticket">
+  <div class="ingresso-comprado__div__ticket__header">
+    <div class="ingresso-comprado__div__ticket__header__logo"></div>
+    <div class="ingresso-comprado__div__ticket__header__icon"></div>
+  </div>
+  <div class="ingresso-comprado__div__ticket__qr-code"></div>
+  <h1 class="ingresso-comprado__div__ticket__nome">${infoIngresso.nome}</h1>
+  <h4 class="ingresso-comprado__div__ticket__tipo-ingresso">Ingresso</h4>
+  <h4 class="ingresso-comprado__div__ticket__setor">Setor ${setorAjustado}</h4>
+  <h4 class="ingresso-comprado__div__ticket__data">Data: ${diaHoje}/${mesHoje}/${dataHoje.getUTCFullYear()}</h4>
+  <h4 class="ingresso-comprado__div__ticket__local">São Paulo-SP</h4>
+</div>
 `
